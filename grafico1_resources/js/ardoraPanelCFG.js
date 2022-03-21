a1210ba8e7ae2fd1d503c5f59220e36e9c1f0f6c
Neeeeear <hectorraulsortoparada@gmail.com>
2022-03-21 11:44:47 -0600

    Examen
    Gráfico de partes de una computadora, Infórmatica Educativa.

--- grafico1_resources/js/ardoraPanelCFG.js
+++ grafico1_resources/js/ardoraPanelCFG.js
@@ -27,6 +27,6 @@ var texSel=0; var doneA=["0","0","0","0","0","0","0"];
 var posX=[10,187,355,479,34,199,388]; var posY=[393,399,397,370,39,40,41];
 var coorx=["MTE1", "MjM1", "MjU1", "MzYw", "NDk1", "NTE0", "NDEy"]; var coory=["MTU3", "MzE2", "MTUx", "Mjk5", "Mjgz", "MTUz", "MTQ4"]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
 var al1=["", "", "", "", "", "", ""]; var al2=["", "", "", "", "", "", ""]; var al3=["", "", "", "", "", "", ""]; var al4=["", "", "", "", "", "", ""]; answers=["CPU", "Teclado", "Monitor", "Mouse", "Impresor", "Cámara", "Bocinas"];indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
-var wordsGame="Z3JhZmljbw=="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+var wordsGame="Z3JhZmljbzE="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
 for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
